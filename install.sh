5d2dfcb3d6bc4d6c57852f668257718bbc0df748
Marc-André Laverdière <marc-andre@atc.tcs.com>
2012-10-31 11:45:43 -0400

    Fixed installer if missing directories

--- install.sh
+++ install.sh
@@ -7,13 +7,16 @@ else
 fi
 
 # KDE Color Palette
+mkdir -p ${kde_path}/share/config/colors/
 cp ./Solarized_Colors ${kde_path}/share/config/colors/Solarized\ Colors
 
 # Konsole (yakuake, etc.) color scheme
+mkdir -p ${kde_path}/share/apps/konsole/
 cp ./"Solarized Dark.colorscheme"  ${kde_path}/share/apps/konsole/
 cp ./"Solarized Light.colorscheme" ${kde_path}/share/apps/konsole/
 
 # Kate Color Schema and syntax highligting
+mkdir -p ${kde_path}/share/config/
 cat ./"Solarized Dark.kateschema" >>   ${kde_path}/share/config/kateschemarc
 cat ./"Solarized Dark.katesyntax" >>   ${kde_path}/share/config/katesyntaxhighlightingrc
 cat ./"Solarized Light.kateschema" >>  ${kde_path}/share/config/kateschemarc
